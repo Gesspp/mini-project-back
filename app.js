2e39addb89420db67ed732308f25dfdb6f53186c
Gesspp <nikitatum199@gmail.com>
2024-04-16 19:08:37 +0300

    fix connection

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use("/tasks", taskRouter);
 
 async function main() {
     try {
-        await mongoose.connect("mongodb://http://127.0.0.1:27017/tasks");
+        await mongoose.connect("mongodb://127.0.0.1:27017/tasks");
         app.listen(3000);
     }
     catch(err) {
