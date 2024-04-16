89167ec1d72e00766ecaceb6fefa0622ba7e5cc2
Gesspp <nikitatum199@gmail.com>
2024-04-16 18:56:24 +0300

    port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 async function main() {
     try {
         await mongoose.connect("mongodb://http://127.0.0.1:27017/task");
-        app.listen(3001);
+        app.listen(3000);
     }
     catch(err) {
         return console.log(err);
