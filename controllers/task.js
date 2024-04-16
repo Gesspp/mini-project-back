58269d5d5531e64ddd1970b6bc6779a4cf86d7f6
Gesspp <nikitatum199@gmail.com>
2024-04-16 19:28:36 +0300

    функции

--- controllers/task.js
+++ controllers/task.js
@@ -4,14 +4,56 @@ const add = async(req, res) => {
     if (!req.body) {
         return res.status(400).json({error : "no data"});
     }
-    console.log("no data")
 
     const {title} = req.body;
     const task = new Task({title, isDone : false});
     await task.save();
-    return res.status(201).json("задача создана");
+    return res.status(201).json({"message" : "задача создана"});
+}
+
+const all = async(req, res) => {
+    const tasks = await Task.find();
+    return res.status(200).json({tasks});
+}
+
+const notDone = async(req, res) => {
+    const tasks = await Task.find({isDone : false});
+    return res.status(200).json({tasks});
+}
+
+const makeDone = async(req, res) => {
+    if (!req.body) {
+        return res.status(400).json({error : "no data"});
+    }
+    const {id} = req.body;
+    const task = await Task.findById(id);
+    task.isDone = true;
+    await task.save();
+
+    return res.status(200).json({"message" : "задача выполнена"});
+    
+}
+
+const clearTasks = async(req, res) => {
+    await Task.deleteMany({isDone : true});
+    
+    return res.status(200).json({"message" : "задачи успешно удалены"});
+}
+
+const deleteById = async(req, res) => {
+    const {id} = req.body;
+
+    const task = await Task.findById(id);
+    await Task.deleteOne(task);
+
+    return res.status(200).json({"message" : "задача успешно удалена"});
 }
 
 module.exports = {
-    add
+    add,
+    all,
+    notDone,
+    makeDone,
+    clearTasks,
+    deleteById
 }
--- routers/task.js
+++ routers/task.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
-const { add } = require("../controllers/task");
+const { add, all, notDone, makeDone, clearTasks, deleteById } = require("../controllers/task");
 
 const router = Router();
 
 router.post("/", add);
+router.get("/", all);
+router.get("/notdone", notDone);
+router.put("/", makeDone);
+router.delete("/", clearTasks);
+router.delete("/deletebyid", deleteById);
 
 module.exports = router;
